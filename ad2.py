c523db2a21487a7c059bef7bf58fc56902f9af46
naja000 <110969361+naja000@users.noreply.github.com>
2022-08-10 13:51:48 +0530

    Add files via upload

---
+++ ad2.py
@@ -0,0 +1,12 @@
+remove=[1,2,3,4,5,"Hello"]
+print(remove)
+remove.remove(1)
+print(remove)
+remove.remove(2)
+print(remove)
+remove.pop(0)
+print(remove)
+remove.pop()
+print(remove)
+del remove[0]
+print(remove)
